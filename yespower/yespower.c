88e3b4f245647ffba8cb42f6ff8468cbaaabfb7b
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-07-28 15:20:15 +0900

    Update yespower.c

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -6,6 +6,7 @@ static const yespower_params_t v1 = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
 static const yespower_params_t v2 = {YESPOWER_1_0, 2048, 32, NULL, 0};
 
 int yespower_hash(const char *input, char *output)
+void yespower_hash(const char *input, char *output)
 {
     uint32_t time = le32dec(&input[68]);
     if (time > 1530403200) {
