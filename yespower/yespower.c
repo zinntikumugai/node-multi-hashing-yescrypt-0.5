ca531141ae5d76917608ed02461d65c2fa0e5894
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-07-28 17:32:56 +0900

    Update yespower.c

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -3,4 +3,4 @@
 
 static const yespower_params_t v1 = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
 
-static const yespower_params_t v2 = {YESPOWER_1_0, 2048, 32, NULL, 0};
+static const yespower_params_t v2 = {YESPOWER_0_9, 2048, 32, NULL, 0};
